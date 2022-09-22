a56bf856231c518ef9e64149e39aea1cd824b0c5
Justin <Justin.Swatloski@gmail.com>
2022-09-22 17:59:07 -0500

    Working iterator storage in array

--- src/Stringer.java
+++ src/Stringer.java
@@ -1,16 +1,25 @@
 public class Stringer {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
 
         int substring = 3;
 
 
-        String test = "ABCDEFGES";
+        String test = "ABCDE";
+
         // "itLen" stands for Iterator Length, here to find the amount of times one has to iterate over a string
         //  to check every character.
         int itLen = test.length() - substring+1;
+        // Create an array of substrings to store every iteration in for comparison later
+        String[] subStrings = new String[itLen];
+
         for(int i = 0; i<itLen; i++){
-            System.out.println(test.substring(i, substring+i));
+
+
+            subStrings[i] = test.substring(i,substring+i);
+
         }
+
+        System.out.println("Value at 0 is: " + subStrings[2]);
     }
 }
